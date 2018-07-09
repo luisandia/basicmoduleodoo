a62d254bb516c6c0d5b5bb2b670b09df355ccc70
Andial <luisandiamamani@gmail.com>
2018-07-09 00:10:24 -0500

    added todo_user: inherit

---
+++ todo_user/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'Multiuser To-Do',
+    'description': 'Extend To-Do Tasks for multiuser',
+    'author': 'Luis Andia',
+    'depends': ['todoapp', 'mail'],
+    'data': ['views/todo_task.xml', 'security/todo_access_rules.xml'], 
+
+}
