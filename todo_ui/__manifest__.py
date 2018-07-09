b28e819cb892a7c499da23c8663bce5bd0ac55a8
jheisson <jheisson.rojas@ucsp.edu.pe>
2018-07-09 18:27:47 -0500

    added: todo_ui

---
+++ todo_ui/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    'name': 'User interface improvements to the To-Do app',
+    'description': 'User friendly features.',
+    'author': 'Luis Andia',
+    'depends': ['todo_user'],
+
+}
--- todo_user/__manifest__.py
+++ todo_user/__manifest__.py
@@ -3,6 +3,10 @@
     'description': 'Extend To-Do Tasks for multiuser',
     'author': 'Luis Andia',
     'depends': ['todoapp', 'mail'],
-    'data': ['views/todo_task.xml', 'security/todo_access_rules.xml'], 
-
+    'data': ['views/todo_task.xml', 'security/todo_access_rules.xml'],
+    'demo': [
+            # Ch04 Data files
+        'data/todo.task.csv',
+        'data/todo_data.xml',
+    ],
 }
